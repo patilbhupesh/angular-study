2561007af6dd86bd428fb34eae50bcfa32559203
Akanksha.Kapade <aakanksha.mk27@gmail.com>
2021-05-24 13:23:16 +0530

    commit on aakansha

--- second-angular-app/src/app/app.module.ts
+++ second-angular-app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { SubtitleComponent } from './subtitle/subtitle.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ArticleComponent } from './article/article.component';
+import { ListComponent } from './list/list.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    SubtitleComponent,
+    SidebarComponent,
+    ArticleComponent,
+    ListComponent,
+    ParagraphComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
